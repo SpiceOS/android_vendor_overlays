SuperiorOS/android_packages_overlays_Themes

Themes: Bring back UDFPS icons and animations [3/3]

themes.mk
@@ -169,3 +169,9 @@ PRODUCT_PACKAGES += \
 # Themes
 PRODUCT_PACKAGES += \
     AndroidBlackThemeOverlay
+
+# UDFPS Animations
+ifeq ($(SUPERIOR_UDFPS_ANIMATIONS),true)
+PRODUCT_PACKAGES += \
+    UdfpsResources
+endif

SpiceOS/android_vendor_overlays

Signed-off-by: Anush02198 <anush.4376@gmail.com>

themes.mk
@@ -157,3 +157,9 @@ PRODUCT_PACKAGES += \
 # Themes
 PRODUCT_PACKAGES += \
     AndroidBlackThemeOverlay
+
+# UDFPS Animations
+ifeq ($(SPICEOS_UDFPS_ANIMATIONS),true)
+PRODUCT_PACKAGES += \
+    UdfpsResources
+endif